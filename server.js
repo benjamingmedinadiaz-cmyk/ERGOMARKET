5f233e10fb0b8aa187a8e7fe1938a3b3483519df
Benjamin <benjamingmedinadiaz@gmail.com>
2026-05-06 21:51:57 -0300

    fix render port

--- server.js
+++ server.js
@@ -139,6 +139,8 @@ app.post("/checkout", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Servidor corriendo en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
